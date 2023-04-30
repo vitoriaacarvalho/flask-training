95567cc22365758871a5b8a1f32f83d9967f3f6f
vitoria <vitoriabcarvalhocorrea@gmail.com>
2023-04-30 18:06:09 -0300

    using personalized responses

--- car_api/main.py
+++ car_api/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify, request
 from data import Cars
 
 app = Flask(__name__)
@@ -6,7 +6,18 @@ app = Flask(__name__)
 
 @app.route('/cars', methods=['GET'])
 def get_cars():
-    return Cars
+    return make_response(
+        jsonify(Cars)
+    )
+# make_response is used to create a personalized response (when we dont use it flask returns a tuple
+# jsonify is used to serialize objects and return them as http responses
+
+
+@app.route('/cars', methods=['POST'])
+def create_car():
+    car = request.json
+    Cars.append(car)
+    return car
 
 
 app.run()
